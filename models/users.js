8f246d4c8109f31248e4f56b3c9c86075658db53
Nour Fares <98664707+Nourfaress@users.noreply.github.com>
2022-09-30 18:09:29 +0200

    First commit

---
+++ models/users.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const becrypt = require('bcryptjs');
+const config = require('../config/database');
+
+//User Schema
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+const User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.getUserById = function(id, callback){
+    User.findById(id, callback);
+}
+
+module.exports.getUserByUsername = function(username, callback){
+    const query = {username: username}
+    User.findByUsername(username, callback);
+}
+
+module.exports.addUser = function(newUser, callback){
+    becrypt.genSalt(10, (err, salt) => {
+        becrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser.save(callback);
+        });
+    });
+}
